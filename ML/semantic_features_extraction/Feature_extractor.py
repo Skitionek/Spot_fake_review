ee35816d3dc478c65fcfab55482ec98397151445
Dominik Maszczyk <Skitionek@gmail.com>
2017-05-26 21:38:35 +0900

    Updated to python3

--- ML/semantic_features_extraction/Feature_extractor.py
+++ ML/semantic_features_extraction/Feature_extractor.py
@@ -1,5 +1,3 @@
-#!/usr/local/bin/python2.7
-# encoding: utf-8
 import csv
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
@@ -34,41 +32,41 @@ class Feature_extractor(object):
     def get_contents(self,fake):
         self.review_list = []
 
-        with open('../Data/YelpZip/reviewContent','rb') as reviews, open('../Data/YelpZip/metadata','rb') as metadatas:
+        with open('../../Data/YelpZip/reviewContent','rt') as reviews, open('../../Data/YelpZip/metadata','rt') as metadatas:
             reviews = csv.reader(reviews, delimiter='\t')
             metadatas = csv.reader(metadatas, delimiter='\t')
          
-            for review,metadata in itertools.izip(reviews, metadatas):
+            for review,metadata in izip(reviews, metadatas):
                 if (metadata[3] == str(-fake)):
                     self.review_list.append(review[3])
                          
-        print "Get-Contents done"
+        print("Get-Contents done")
 
     def vectorize(self,ngram):
         vectorizer = CountVectorizer(max_df=0.95,min_df=10,ngram_range=(ngram,ngram),analyzer='word',stop_words='english',max_features=100,token_pattern=r"(?u)\b[a-zA-Z]+'?[a-zA-Z]+\b",strip_accents='ascii')
         
         self.X = vectorizer.fit_transform(self.review_list).toarray()
         self.names = vectorizer.get_feature_names()
-        print len(self.names)
+        print(len(self.names))
         
-        print "Vectorize done"
+        print("Vectorize done")
         self.print_X()
 
     def transform(self):
         transformer = TfidfTransformer(smooth_idf=False)
         self.X = transformer.fit_transform(self.X).toarray()
         
-        print "Transform done"
+        print("Transform done")
         self.print_X()
         
     def save(self, fileName):
-        with open('../Data/Extracted_features/'+fileName+'.npy', 'wb') as r:
+        with open('../../Data/Extracted_features/'+fileName+'.npy', 'wb') as r:
            
             np.save(r, np.array(self.X))
             
-        print "Saving done"     
+        print("Saving done")   
     
     def print_X(self):
-        print self.names
-        print self.X
+        print(self.names)
+        print(self.X)
         
