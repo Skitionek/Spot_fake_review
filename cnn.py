4c45a81d98daf1e967d2c886e64fe0480839e24c
warmdaylight <settasit@kaist.ac.kr>
2017-05-28 23:34:19 +0900

    Handle Imbalance classes

--- cnn.py
+++ cnn.py
@@ -1,14 +1,7 @@
 from sklearn.neural_network import MLPClassifier
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedKFold
 import numpy as np
 import sys
-# from svm_behav import y_train
-
-
-
-import csv
-
-
 
 """
     cnn.py
@@ -42,50 +35,51 @@ class trainingModel:
     def classify(self, X):
         return self.model.predict(X)
 
-    def validate(self, X, Y):
-        return self.model.score(X, Y)
-        # compare
-        # return float(np.count_nonzero(result == Y)) / float(len(Y))
+    def validate(self, X, idx):
+        data = self.model.predict_proba(X)[:, idx]
+        return np.mean(self.model.predict(X)), np.mean(data), np.std(data)
 
 
 def main(mode):
     # option
-    print("mode: ", sys.argv[1])
+    print("mode: {0}".format(sys.argv[1]))
 
     # load
     features = load(mode)
 
-
     cla = np.array(np.loadtxt('Data/YelpZip/metadata',usecols=[3], dtype='string', delimiter='\t'))
                 
     cla = (cla == '1')
-    
-    # cross validation
-    X_train, X_test, Y_train, Y_test = train_test_split(features, cla, test_size=0.4, random_state=0)
+
+    print("real: {0}\nfake: {1}\n".format(np.count_nonzero(cla), len(cla) - np.count_nonzero(cla)))
 
     print("test loaded")
+    
+    # cross validation
+    splitter = StratifiedKFold(n_splits=3)
+    for train_idx, test_idx in splitter.split(features, cla):
+        X_train = features[train_idx]
+        X_test = features[test_idx]
+        Y_train = cla[train_idx]
+        Y_test = cla[test_idx]
 
-    # training model
-    m = trainingModel()
+        # training model
+        m = trainingModel()
 
-    # train
-    m.train(X_train, Y_train)
+        # train
+        m.train(X_train, Y_train)
 
-    print("trained")
+        print("trained")
 
-    # validate
-    print("Overall precision : ", m.validate(X_test, Y_test))
+        # partition
+        fake_test = X_test[- Y_test]
+        real_test = X_test[Y_test]
 
-    # partition
-    fake_test = X_test[- Y_test]
-    Y_test_fake = np.array([False] * len(fake_test))
-    real_test = X_test[Y_test]
-    Y_test_real = np.array([True] * len(real_test))
-
-    print("Fake review precision: ", m.validate(fake_test, Y_test_fake))
-    print("Real review precision: ", m.validate(real_test, Y_test_real))
-
-    return m
+        # validation
+        acc, prob, sd = m.validate(fake_test, 0)
+        acc = 1 - acc
+        print("Fake review (precision, prob, sd): {0}".format((acc, prob, sd)))
+        print("Real review (precision, prob, sd): {0}".format(m.validate(real_test, 1)))
 
 
 if __name__ == '__main__':
